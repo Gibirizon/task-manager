b8186c43bb968c55d4a202116f27ac7501055a9a
Gibirizon <swojteksz@gmail.com>
2024-05-19 23:57:14 +0200

    New table for checking realistion of plan from three screens

--- database/plan_realisation_table.py
+++ database/plan_realisation_table.py
@@ -4,7 +4,7 @@ from datetime import date
 # create table for daily tasks
 def create_plan_realisation_table(self):
     self.cur.execute(
-        f"CREATE TABLE IF NOT EXISTS plan_realisation (day date PRIMARY KEY, plan_name varchar(20) NOT NULL, all_completed BOOLEAN NOT NULL CHECK (all_completed IN (0, 1)))"
+        f"CREATE TABLE IF NOT EXISTS plan_realisation (id integer PRIMARY KEY AUTOINCREMENT, day date NOT NULL, plan_name varchar(20) NOT NULL, all_completed BOOLEAN NOT NULL CHECK (all_completed IN (0, 1)))"
     )
     self.con.commit()
 
@@ -13,14 +13,23 @@ def create_plan_realisation_table(self):
 def create_or_change_item_realisation(self, plan_name, all_completed):
     # get current date
     current_date = date.today()
-    print(current_date)
     # checking if this option already exists
-    self.cur.execute(
-        f"INSERT INTO plan_realisation(day, plan_name, all_completed) VALUES(?,?,?)",
-        (
-            current_date,
-            plan_name,
-            all_completed,
-        ),
-    )
+    today_elements = self.cur.execute(
+        f"SELECT id FROM plan_realisation WHERE day=? AND plan_name=?",
+        (current_date, plan_name),
+    ).fetchone()
+    if today_elements:
+        self.cur.execute(
+            f"UPDATE plan_realisation SET all_completed=? WHERE id=?",
+            (all_completed, today_elements[0]),
+        )
+    else:
+        self.cur.execute(
+            f"INSERT INTO plan_realisation(day, plan_name, all_completed) VALUES(?,?,?)",
+            (
+                current_date,
+                plan_name,
+                all_completed,
+            ),
+        )
     self.con.commit()
--- modules/stats/stats.py
+++ modules/stats/stats.py
@@ -14,35 +14,37 @@ class Stats(Screen):
         super().__init__(**kw)
 
     def tabs_menu_open(self):
+        menu_elements_info = [
+            ["checkbox-marked-circle-auto-outline", "tasks", "Daily Tasks"],
+            ["timeline-plus", "goals", "Daily Goals"],
+            ["calendar-week", "weekly_tasks", "Weekly Tasks"],
+        ]
         menu_items = [
             {
-                "text": "Daily Tasks",
-                "leading_icon": "checkbox-marked-circle-auto-outline",
+                "text": element[2],
+                "leading_icon": element[0],
                 "leading_icon_color": "orange",
-            },
-            {
-                "text": "Goals",
-                "leading_icon": "timeline-plus",
-                "leading_icon_color": "orange",
-            },
-            {
-                "text": "Weekly Tasks",
-                "leading_icon": "calendar-week",
-                "leading_icon_color": "orange",
-            },
+                "on_release": lambda text=element[2], table_name=element[
+                    1
+                ]: self.calculate_statistics(text, table_name),
+            }
+            for element in menu_elements_info
         ]
         self.tabs_menu = MDDropdownMenu(caller=self.ids.btn, items=menu_items).open()
 
-    # def on_pre_enter(self, *args):
-    #     self.count_statistics("tasks")
-    #     return super().on_pre_enter(*args)
+    def on_pre_enter(self, *args):
+        self.calculate_statistics("Daily tasks", "tasks")
+        return super().on_pre_enter(*args)
 
-    # def count_statistics(self, table_name):
-    #     # get all items of the table which was chosen from dropdown list (by default daily tasks)
-    #     all_elements = app.root.db.get_all_items(table_name)
+    def calculate_statistics(self, text, table_name):
+        # change button text
+        self.ids.btn_text.text = text
 
-    #     # check if all of the planned tasks are completed
-    #     completed = all([x[-1] for x in all_elements])
+        # get all items of the table which was chosen from dropdown list (by default daily tasks)
+        all_elements = app.root.db.get_all_items(table_name)
 
-    #     # add information to a database
-    #     app.root.db.create_or_change_item_realisation(table_name, int(completed))
+        # check if all of the planned tasks are completed
+        completed = all([x[-1] for x in all_elements])
+
+        # add information to a database
+        app.root.db.create_or_change_item_realisation(table_name, int(completed))
